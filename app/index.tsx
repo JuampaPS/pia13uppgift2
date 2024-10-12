df7aac31b274fdd990ccb9f516f1f70891848525
Juan Pablo Palacio Soler <juampa_ps@icloud.com>
2024-10-12 19:19:28 +0200

    almost ready

---
+++ app/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+
+  const [counter, setCounter] = useState(0);
+  const [mynumber, setMynumber] = useState("");
+  const [errormessage, setErrormessage] = useState("");
+
+
+
+
+
+  function dosomeplus() {
+
+    const innumb = Number(mynumber);
+
+    if (isNaN(innumb)) {
+      // Inte siffra
+      setErrormessage("Du skrev fel!!!");
+    } else {
+      setErrormessage("");
+      setCounter(counter + innumb);
+
+      if (counter > 10) {
+        setCounter(0);
+      }
+
+    }
+
+
+
+  }
+
+  function dosomeminus() {
+    setCounter(counter - 1);
+
+    if (counter < 0) {
+      setCounter(0);
+    }
+  }
+
+
+
+  return (
+
+    
+    
+    <View
+
+
+      style={{
+        flex: 1,
+        justifyContent: "up",
+        alignItems: "center",
+      }}
+    >
+       <Text>{counter}</Text> 
+
+      {errormessage != "" &&
+
+        <Text style={{ borderWidth: 3, width: 200, height: 80, }}>{errormessage}</Text>
+      }
+      <TextInput
+        onChangeText={setMynumber}
+        value={mynumber}
+        style={{ borderWidth: 1, width: 100 }} />
+
+
+
+      <Button
+        onPress={dosomeplus}
+        title="PLUS"
+      />
+
+      
+        <Button
+          onPress={dosomeminus}
+          title="MINUS"
+        />
+      
+      {counter > 0 &&
+      <Button
+        onPress={() => { setCounter(0); }}
+        title="RESET"
+      />
+      }
+
+
+    </View>
+
+
+
+
+  );
+}
